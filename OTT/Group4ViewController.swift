4e41e1a8af85b76c46ee1e226b225b0ec33781b2
cpuu <cpuu@icloud.com>
2016-06-07 14:24:19 +0900

    친구추가 화면 구성

---
+++ OTT/Group4ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  Group4ViewController.swift
+//  OTT
+//
+//  Created by 박재유 on 2016. 6. 7..
+//  Copyright © 2016년 Troy Stribling. All rights reserved.
+//
+
+import UIKit
+
+class Group4ViewController: UIViewController {
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        //print("Second VC will appear")
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        //print("Second VC will disappear")
+    }
+}
+
