eb8bbc017eacf02240e8a89da674f867ed52507b
cpuu <cpuu@icloud.com>
2016-06-18 17:37:37 +0900

    뇌구조 디비 연동 완료

--- OTT/Statistics1ViewController.swift
+++ OTT/Statistics1ViewController.swift
@@ -9,11 +9,57 @@
 import UIKit
 
 class Statistics1ViewController: UIViewController {
+    @IBOutlet weak var seventh: UILabel!
+    @IBOutlet weak var eighth: UILabel!
+    @IBOutlet weak var second: UILabel!
+    @IBOutlet weak var sixth: UILabel!
+    @IBOutlet weak var fifth: UILabel!
+    @IBOutlet weak var fourth: UILabel!
+    @IBOutlet weak var third: UILabel!
+    @IBOutlet weak var first: UILabel!
 
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
-        //print("First VC will appear")
+    @IBOutlet weak var onmymind: UILabel!
+    
+    var memberData : BrainMember!
+    var marrMemberData : NSMutableArray!
+    
+    func getMemberData()
+    {
+        marrMemberData = NSMutableArray()
+        marrMemberData = ModelManager.getInstance().getBrainAllData()
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        self.getMemberData()
+        
+        var brain:BrainMember = marrMemberData.objectAtIndex((0)) as! BrainMember
+        self.first.text! = brain.GROUP_MEMBER_NM
+        self.onmymind.text! = brain.GROUP_MEMBER_NM + " is on my mind."
+        
+        brain = marrMemberData.objectAtIndex((1)) as! BrainMember
+        self.second.text! = brain.GROUP_MEMBER_NM
+        
+        brain = marrMemberData.objectAtIndex((2)) as! BrainMember
+        self.third.text! = brain.GROUP_MEMBER_NM
+        
+        brain = marrMemberData.objectAtIndex((3)) as! BrainMember
+        self.fourth.text! = brain.GROUP_MEMBER_NM
+        
+        brain = marrMemberData.objectAtIndex((4)) as! BrainMember
+        self.fifth.text! = brain.GROUP_MEMBER_NM
+        
+        brain = marrMemberData.objectAtIndex((5)) as! BrainMember
+        self.sixth.text! = brain.GROUP_MEMBER_NM
+        
+        brain = marrMemberData.objectAtIndex((6)) as! BrainMember
+        self.seventh.text! = brain.GROUP_MEMBER_NM
+        
+        brain = marrMemberData.objectAtIndex((7)) as! BrainMember
+        self.eighth.text! = brain.GROUP_MEMBER_NM
+        
+        super.viewWillAppear(animated)
+    }
+    
     
 
     override func viewWillDisappear(animated: Bool) {
